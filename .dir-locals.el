6a87d578a33607a42b5dc28f5f2fc8e9f5218f4c
Denis Zubarev <dvzubarev@yandex.ru>
2024-07-24 19:58:49 +0300

    dev env

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((projectile-project-compilation-dir . "build")
+         (helm-make-build-dir . "build")
+         (projectile-project-name . "DiskAnn")
+         (projectile-project-configure-cmd .
+"cmake \
+-DFAISS_ENABLE_GPU=OFF -DFAISS_ENABLE_PYTHON=OFF \
+-DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..")
+         )))
